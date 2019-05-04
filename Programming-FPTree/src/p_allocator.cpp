2a663a9d7858ea6663a97f6fc6bc8d9bd7cf51e4
anineee <49850164+anineee@users.noreply.github.com>
2019-05-04 21:29:18 +0800

    Update p_allocator.cpp

--- Programming-FPTree/src/p_allocator.cpp
+++ Programming-FPTree/src/p_allocator.cpp
@@ -31,6 +31,14 @@ PAllocator::PAllocator() {
     if (allocatorCatalog.is_open() && freeListFile.is_open()) {
         // exist
         // TODO
+        allocatorCatalog.read((char*)&this->maxFileId, sizeof(uint64_t));
+        allocatorCatalog.read((char*)&this->freeNum, sizeof(uint64_t));
+        allocatorCatalog.read((char*)&this->startLeaf, sizeof(PPointer));
+        for(int i = 0; i < this->freeNum; i ++) {
+            PPointer tmp;
+            freeListFile.read((char*)&tmp, sizeof(PPointer));
+            this->freeList.push_back(tmp);
+        }
     } else {
         // not exist, create catalog and free_list file, then open.
         // TODO
@@ -42,14 +50,26 @@ PAllocator::~PAllocator() {
     // TODO
 }
 
+const uint64_t LEAF_GROUP_SIZE = LEAF_GROUP_HEAD + LEAF_GROUP_AMOUNT*calLeafSize();
+
 // memory map all leaves to pmem address, storing them in the fId2PmAddr
 void PAllocator::initFilePmemAddr() {
     // TODO
+    for(int i = 1; i < this->maxFileId; i ++) {
+        size_t mapped_lenp;
+        int is_pmemp;
+        string leafPath = DATA_DIR + to_string(i);
+        char* pmem_addr = pmem_map_file((const char *)leafPath, LEAF_GROUP_SIZE, PMEM_FILE_CREATE,
+    0666, &mapped_lenp, &is_pmemp);
+        this->fId2PmAddr.insert(pair<uint64_t, char*>(i, pmem_addr));
+    }
 }
 
 // get the pmem address of the target PPointer from the map fId2PmAddr
 char* PAllocator::getLeafPmemAddr(PPointer p) {
     // TODO
+    map<uint64_t, char*>::iterator it = this->fId2PmAddr(p.fileId);
+    if(it != this->fId2PmAddr.end()) return it->second;
     return NULL;
 }
 
@@ -57,27 +77,50 @@ char* PAllocator::getLeafPmemAddr(PPointer p) {
 // return 
 bool PAllocator::getLeaf(PPointer &p, char* &pmem_addr) {
     // TODO
+    if(this->freeNum != 0) {
+        p = this->freeList[this->freeNum-1];
+        this->freeList.pop_back();
+        this->freeNum --;
+        pmem_addr = this->fId2PmAddr.find(p.fileId).second;
+        string allocatorCatalogPath = DATA_DIR + P_ALLOCATOR_CATALOG_NAME;
+        string freeListPath = DATA_DIR + P_ALLOCATOR_FREE_LIST;
+        string leafgroupPath = DATA_DIR + to_string(p.fileId);
+         
+        return true;
+    }
     return false;
 }
 
 bool PAllocator::ifLeafUsed(PPointer p) {
     // TODO
+    if(!this->ifLeafExist(p)) return false;
+    if(!ifLeafFree(p)) return true;
     return false;
 }
 
 bool PAllocator::ifLeafFree(PPointer p) {
     // TODO
+    if(!this->ifLeafExist(p)) return false;
+    for(vector<PPointer>::iterator it = this->freeList.begin(); it != this->freeList.end(); it ++) {
+        if(p == it) return true;
+    }
     return false;
 }
 
 // judge whether the leaf with specific PPointer exists. 
 bool PAllocator::ifLeafExist(PPointer p) {
     // TODO
+    if(p.fileId >= this->maxFileId) return false;
+    if(p.offset > LEAF_GROUP_SIZE-calLeafSize()) return false;
+    return true;
 }
 
 // free and reuse a leaf
 bool PAllocator::freeLeaf(PPointer p) {
     // TODO
+    if(this->ifLeafUsed(p)) {
+
+    }
     return false;
 }
 
